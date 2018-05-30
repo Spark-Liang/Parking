75fc7a2032419f4fbb78399f6977c8be0b93fae0
LBH520abc <754654996@qq.com>
2018-05-30 11:00:45 +0800

    修改了登录验证，增加用户登录页面userLogin.jsp

--- src/main/webapp/js/check6.js
+++ src/main/webapp/js/check6.js
@@ -1,5 +1,5 @@
 //账号检测
-function checkAdmin(className,tip){
+/*function checkAdmin(className,tip){
         var name = document.getElementsByClassName(className)[0];
         var uName = name.value.trim();
         var nameLength = uName.length;
@@ -23,23 +23,23 @@ function checkAdmin(className,tip){
         }
         return true;
     }
+*/
 
-
-//持卡普通用户账号检测
+//用户账号检测
 function checkName(className,tip){
         var name = document.getElementsByClassName(className)[0];
         var uName = name.value.trim();
         var nameLength = uName.length;
-        var regex = new RegExp("^[0-9a-zA-Z]+$");
+        var object = /^1{1}[3-9]{2}[0-9]{8}$/g;
         if (nameLength < 1) {
             tip.innerHTML = "请输入账号";
             name.focus();
             return false;
-        }else if(nameLength > 16){
-            tip.innerHTML = '账号长度不能超出16位';
+        }else if(!object.test(uName)){
+            tip.innerHTML = '手机号码格式有问题';
             name.focus();
             return false;
-        }else if(uName.indexOf(' ') !== -1){
+        }/*else if(uName.indexOf(' ') !== -1){
             tip.innerHTML = '账号中间不能有空格';
             name.focus();
             return false;
@@ -47,7 +47,7 @@ function checkName(className,tip){
             tip.innerHTML = "账号只能包含英文和数字";
             name.focus();
             return false;
-        }
+        }*/
         return true;
     }
 //密码检测
@@ -56,11 +56,17 @@ function checkPwd(className,tip){
     var pass = document.getElementsByClassName(className)[0];
     var passValue = pass.value.trim();
     var passLength = passValue.length;
+   
+    
     if(passLength < 1){
         tip.innerHTML = "请输入密码";
         pass.focus();
         return false;
     
+    }else if(passLength < 6){
+        tip.innerHTML = "密码长度要大于6位";
+        pass.focus();
+        return false;
     }else if(passValue.indexOf(' ') !== -1){
         tip.innerHTML = '密码中间不能有空格';
         pass.focus();
